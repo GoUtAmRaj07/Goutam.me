06ddb82679fc685a694c88d503703d6df1dfbd2d
Goutam Raj <74093499+GoUtAmRaj07@users.noreply.github.com>
2022-06-30 19:46:08 +0530

    modified files

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div className="cta">
         {/* <a href={CV} download className='btn'>Download CV</a> */}
-        <a href="https://drive.google.com/file/d/1-dCQb2JVmxXT97coAsLUDHFfeG01w-kf/view?usp=sharing" download className='btn' target="_blank">View CV</a>
+        <a href="https://drive.google.com/file/d/1-fWprQrge7scXxbx4fQIfS04VgAEIBTa/view?usp=sharing" download className='btn' target="_blank">View CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
