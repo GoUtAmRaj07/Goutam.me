979964b630985b6e7d97e639816b2cb58c05e2f5
Goutam Raj <74093499+GoUtAmRaj07@users.noreply.github.com>
2022-06-30 11:08:54 +0530

    modified files

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import CV from '../../assets/CV.pdf'
+// import CV from '../../assets/CV.pdf'
 
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV} download className='btn'>Download CV</a>
+        {/* <a href={CV} download className='btn'>Download CV</a> */}
+        <a href="https://drive.google.com/file/d/1-dCQb2JVmxXT97coAsLUDHFfeG01w-kf/view?usp=sharing" download className='btn' target="_blank">View CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
